8b702a94ba89e3b47107f1260f66f6a0101a63b0
akashmunjal <50265641+akashmunjal@users.noreply.github.com>
2025-05-07 15:18:16 +0530

    Add files via upload

---
+++ Assignment 1_Task 1.py
@@ -0,0 +1,7 @@
+x = float(input('Enter the first Number: '))
+y = float(input('Enter the second Number: '))
+
+print("Addition: ", x+y)
+print("Subtraction: ", x-y)
+print("Multiplication: ", x*y)
+print("Division: ", x/y)
---
+++ Assignment 1_Task 2.py
@@ -0,0 +1,4 @@
+x = input('Enter the first Name: ')
+y = input('Enter the last name: ')
+z = x+ " " + y
+print("Welcome, ", z, ", to the Python course")
